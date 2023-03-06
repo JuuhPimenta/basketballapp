d84043838aaa6b3fd15840e21cc2059d9af76d17
JuuhPimenta <juuhpimenta11@gmail.com>
2023-03-06 10:41:17 -0300

    um

---
+++ src/components/ListMarker/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  listMarker: {
+    margin: 10,
+    marginTop: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textMarker: {
+
+    marginLeft: 5,
+    fontSize:20
+
+  }
+  
+})
