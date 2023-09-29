f3bc77cebd2556a058d7e321570b764b85f57507
JuuhPimenta <juuhpimenta11@gmail.com>
2023-09-29 08:37:05 -0300

    28

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -4,8 +4,7 @@ import {LoginNavigation} from "./login.navigation";
 import { useAuth } from "../hooks/auth";
 import { TabNavigation } from './tab.navigations';
 export function Navigation() {
-  const {user} = useAuth();
-  console.log(user)
+  const { user } = useAuth();
   return (
     <NavigationContainer>
       {user?.token ? <TabNavigation /> : <LoginNavigation />}
